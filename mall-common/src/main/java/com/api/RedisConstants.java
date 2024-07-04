5d0f017a75fb3890cbe2e3a8d5b2382bfe32b601
AiJun <2505407859@qq.com>
2024-07-04 18:10:39 +0800

    完成了权限中用户和资源及菜单分配的接口，修改了redis序列化的相关bug

--- mall-common/src/main/java/com/api/RedisConstants.java
+++ mall-common/src/main/java/com/api/RedisConstants.java
@@ -5,7 +5,7 @@ public class RedisConstants {
 
     public final static String REDIS_KEY_ADMIN = "ums:admin";
 
-    public final static long REDIS_KEY_ADMIN_TTL = 90000L;
+    public final static long REDIS_KEY_ADMIN_TTL = 60*5L;
 
     public final static String REDIS_KEY_RESOURCE_LIST = "ums:resourceList";
 }
